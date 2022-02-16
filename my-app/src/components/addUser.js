f433c77a7bf5f5629028416198c67a0f6c944a1a
SafaeQ <safaedani009@gmail.com>
2022-02-16 16:20:58 +0100

    ..

--- my-app/src/components/addUser.js
+++ my-app/src/components/addUser.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
 
 export const AddUser = () => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const submitFn = ()=> {
+        axios.post('http://localhost:9000/', {firstName: firstName, lastName: lastName})
+    }
   return (
     <>
       <main>
@@ -14,16 +21,16 @@ export const AddUser = () => {
         <div class= "form">
             <br/>
                 <label>First Name</label>
-            <input type="text" name="firstName"/>
+            <input type="text" name="firstName" onchange={(e)=>{setFirstName(e.target.value)}}/>
                 <label>Last Name</label>
-            <input type="text" name="lastName"/>
+            <input type="text" name="lastName" onchange={(e)=>{setLastName(e.target.value)}} />
                 <label>User Name</label>
             <input type="text" name="userName"/>
                 <label>Status</label>
             <input type="text" name="status"/>  
             <br/>
 
-            <button type="submit">Submit </button>
+            <button onclick={submitFn} type="submit">Submit </button>
         </div>
     </div>
     </>
