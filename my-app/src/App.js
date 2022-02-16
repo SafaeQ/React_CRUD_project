7853eac7cfcaa4adc261907599d001d3df21b9af
SafaeQ <safaedani009@gmail.com>
2022-02-16 11:54:59 +0100

    ..

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,12 @@ import React from "react";
 import {Home} from './components/Home';
 import {AddUser} from './components/AddUser';
 
+
 function App() {
   return (
     <div className="App">
-    {/* <Router> */}
-      <h1>Nav</h1>
-      {/* <Switch> */}
       <Home/>
       <AddUser/>
-      {/* </Switch> */}
-    {/* </Router> */}
     </div>
   );
 }
