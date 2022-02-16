cc83c438973afb0a0ee04f0b25f668f22753626e
SafaeQ <safaedani009@gmail.com>
2022-02-16 11:25:22 +0100

    ..

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from "react";
+import {Home} from './components/Home';
+import {AddUser} from './components/AddUser';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      {/* <h1>hello world</h1> */}
+      <Home/>
+      <AddUser/>
     </div>
   );
 }
