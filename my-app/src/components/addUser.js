70ae1c08a67c9ad4bd0a7d7fe3bfc9af72604501
SafaeQ <safaedani009@gmail.com>
2022-02-17 09:57:27 +0100

    add some dependisies

--- my-app/src/components/addUser.js
+++ my-app/src/components/addUser.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { useState, useEffect} from 'react'
-import axios from 'axios'
+import { useState, useRef} from 'react'
+// import axios from 'axios'
 
 export const AddUser = () => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const submitFn = ()=> {
-        axios.post('http://localhost:9000/', {firstName: firstName, lastName: lastName})
+    const [firstName, setFirstName] = useState([])
+    // const [lastName, setLastName] = useState("")
+    const firstNameRef = useRef()
+
+    const handleOnSubmit = (e)=> {
+        console.log(e)
+        // axios.post('http://localhost:9000/', {firstName: firstName, lastName: lastName})
     }
   return (
     <>
@@ -21,16 +24,16 @@ export const AddUser = () => {
         <div class= "form">
             <br/>
                 <label>First Name</label>
-            <input type="text" name="firstName" onchange={(e)=>{setFirstName(e.target.value)}}/>
+            <input type="text" name="firstName" ref = {firstNameRef}/>
                 <label>Last Name</label>
-            <input type="text" name="lastName" onchange={(e)=>{setLastName(e.target.value)}} />
+            <input type="text" name="lastName"  />
                 <label>User Name</label>
             <input type="text" name="userName"/>
                 <label>Status</label>
             <input type="text" name="status"/>  
             <br/>
 
-            <button onclick={submitFn} type="submit">Submit </button>
+            <button onclick={handleOnSubmit} type="submit">Submit </button>
         </div>
     </div>
     </>
