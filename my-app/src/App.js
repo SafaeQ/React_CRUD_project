2203d0b3864deafbe49e5cf15e8051654c013d2e
SafaeQ <safaedani009@gmail.com>
2022-02-17 10:36:30 +0100

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,64 @@
 import React from "react";
-import {Routes, Route} from 'react-router-dom'
+// import {Routes, Route} from 'react-router-dom'
 import {Home} from './components/Home';
 import {AddUser} from './components/AddUser';
+import {EditUserForm} from './components/EditUserForm';
+import { useState} from 'react'
 import './App.css';
 
 function App() {
+  const usersData = [
+    {id: 1, firstName: 'safae', lastName: 'zeaze', username: 'saf'},
+    {id: 2, firstName: 'safae2', lastName: 'zeaze', username: 'saf'},
+    {id: 3, firstName: 'safae3', lastName: 'zeaze', username: 'saf'}
+  ];
+
+  const initialFormState = {id:null, firstName: '', lastName: '', username: ''}
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+  const [users, setUsers] = useState(usersData)
+  const [editing, setEditing] = useState(false)
+
+  const editRow = user=> {
+    setEditing(true)
+    setCurrentUser({id: user.id, firstName: user.firstName, lastName: user.lastName, username: user.username})
+  }
+
+  const addUser = user => {
+    user.id = users.length + 1;
+    setUsers([...users, user])
+  }
+
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+
   return (
     <div className="App">
       <h1> CRUD APP </h1>
-      <Routes>
+      <div className= "flex-row">
+    <div className="flex-large">
+      {editing ? (
+        <div>
+            <h2>Edit user</h2>
+            <EditUserForm editing={editing} setEditing={setEditing} currentUser={currentUser}/>
+        </div>
+      ):(
+        <div>
+          <h2>Add user</h2>
+          <AddUser addUser={addUser}/>
+        </div>
+      )}
+    </div>
+    <div className="flex-large">
+        <h2>View all users</h2>
+        <Home users={users} editRow={editRow} deleteUser={deleteUser} />
+    </div>
+  </div>
+      {/* <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='add-user' element={<AddUser />} />
-      </Routes>
-    </div>
+      </Routes> */}
+</div>
   );
 }
 
--- my-app/src/components/addUser.js
+++ my-app/src/components/addUser.js
@@ -1,41 +1,41 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import { useState, useRef} from 'react'
+// import {Link} from 'react-router-dom'
+import { useState} from 'react'
 // import axios from 'axios'
 
-export const AddUser = () => {
-    const [firstName, setFirstName] = useState([])
-    // const [lastName, setLastName] = useState("")
-    const firstNameRef = useRef()
+export const AddUser = (argm) => {
+    const initialFormState = {id:null, firstName: '', lastName: '', username: ''}
+    const [user, setUser] = useState(initialFormState)
 
-    const handleOnSubmit = (e)=> {
-        console.log(e)
+
+    const handleInputChange = (event)=> {
+        const {firstName, value} = event.target
+        setUser({...user, [firstName]: value})
+        // console.log(event)
         // axios.post('http://localhost:9000/', {firstName: firstName, lastName: lastName})
     }
   return (
-    <>
-      <main>
-        <h2>welcome to add user page</h2> 
-      </main>
-      <nav>
-        <Link to='/'>Home</Link>
-      </nav>
-      <div>
-        <div class= "form">
-            <br/>
+    //   <main>
+    //     <h2>welcome to add user page</h2> 
+    //   </main>
+    //   <nav>
+        // <Link to='/'>Home</Link>
+    //   </nav>
+      <form onSubmit={e => {
+          e.preventDefault();
+          if (!user.firstName || !user.lastName || !user.username) return;
+            argm.AddUser(user)
+            setUser(initialFormState)
+          
+      }}>
                 <label>First Name</label>
-            <input type="text" name="firstName" ref = {firstNameRef}/>
+            <input type="text" name="firstName" value={user.firstName} onchange={handleInputChange} />
                 <label>Last Name</label>
-            <input type="text" name="lastName"  />
+            <input type="text" name="lastName" value={user.lastName} onchange={handleInputChange} />
                 <label>User Name</label>
-            <input type="text" name="userName"/>
-                <label>Status</label>
-            <input type="text" name="status"/>  
-            <br/>
+            <input type="text" name="userName" value={user.username} onchange={handleInputChange} />
 
-            <button onclick={handleOnSubmit} type="submit">Submit </button>
-        </div>
-    </div>
-    </>
+            <button type="submit">Submit </button>
+    </form>
   )
 }
